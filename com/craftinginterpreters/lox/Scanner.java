dd8e09d68c36edbd3f5b67a8ac4e422684e0e72b
shinokun <hypatia.tile02021@gmail.com>
2026-04-06 23:25:58 +0900

    remove redundant output

--- com/craftinginterpreters/lox/Scanner.java
+++ com/craftinginterpreters/lox/Scanner.java
@@ -6,9 +6,6 @@ public class Scanner {
   public static List<Token> lex(String source) {
     Lexer lexer = new Lexer(source);
     List<Token> tokens = lexer.scanTokens();
-    for (Token token : tokens) {
-      System.out.printf("token: %s\n", token);
-    }
     return tokens;
   }
 }
