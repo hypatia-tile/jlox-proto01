e86801daab2ae0270082133db1a4a8e19557f93f
shinokun <hypatia.tile02021@gmail.com>
2026-04-11 22:36:00 +0900

    feat: add class declaration

--- com/craftinginterpreters/lox/Interpreter.java
+++ com/craftinginterpreters/lox/Interpreter.java
@@ -125,6 +125,14 @@ class Interpreter implements Expr.Visitor<Object>,
   }
 
   @Override
+  public Void visitClassStmt(Stmt.Class stmt) {
+    environment.define(stmt.name.lexeme, null);
+    LoxClass klass = new LoxClass(stmt.name.lexeme);
+    environment.assign(stmt.name, klass);
+    return null;
+  }
+
+  @Override
   public Object visitAssignExpr(Assign expr) {
     Object value = evaluate(expr.value);
 
