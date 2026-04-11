ad5a94c96d02f9b04f0f42874d81252c84d267fb
shinokun <hypatia.tile02021@gmail.com>
2026-04-11 23:24:11 +0900

    feat: add method support

--- com/craftinginterpreters/lox/Resolver.java
+++ com/craftinginterpreters/lox/Resolver.java
@@ -17,7 +17,8 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
   private enum FunctionType {
     NONE,
-    FUNCTION
+    FUNCTION,
+    METHOD
   }
 
   @Override
@@ -32,6 +33,11 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
   public Void visitClassStmt(Stmt.Class stmt) {
     declare(stmt.name);
     define(stmt.name);
+
+    for (Stmt.Function method : stmt.methods) {
+      FunctionType declaration = FunctionType.FUNCTION;
+      resolveFunction(method, declaration);
+    }
     return null;
   }
 
